20804dd0593638555a0c973feed809c339437a56
luciadcf <luciadelcarmenfuentes@gmail.com>
2020-11-25 22:56:54 +0100

    remove firebase delete token

--- app/src/main/java/es/us/managemyteam/usecase/LogoutUc.kt
+++ app/src/main/java/es/us/managemyteam/usecase/LogoutUc.kt
@@ -1,13 +1,11 @@
 package es.us.managemyteam.usecase
 
-import com.google.firebase.iid.FirebaseInstanceId
 import es.us.managemyteam.repository.UserRepository
 
 class LogoutUc(private val userRepository: UserRepository) {
 
     suspend operator fun invoke() {
         userRepository.logout()
-        FirebaseInstanceId.getInstance().deleteInstanceId()
     }
 
 }
